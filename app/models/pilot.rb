13c0cc9b8845aeb24ba4a002bde9eca640201265
Laura Meireles <laura.meireles1@gmail.com>
2024-09-04 14:50:16 -0300

    Feat: Adding new has many to pilot

--- app/models/pilot.rb
+++ app/models/pilot.rb
@@ -2,6 +2,8 @@
 
 class Pilot < ApplicationRecord
   has_one :ship, dependent: :destroy
+  has_many :contracts
+  
   accepts_nested_attributes_for :ship
 
   validates :pilot_certification, :name, :age, :credits, :location_planet, presence: true
