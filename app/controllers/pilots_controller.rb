0d1d1862c5f0327649c6f78497988e69d01832af
Laura Meireles <laura.meireles1@gmail.com>
2024-09-04 11:39:40 -0300

    Feat: Creating method create to pilot and ship

--- app/controllers/pilots_controller.rb
+++ app/controllers/pilots_controller.rb
@@ -1,4 +1,37 @@
 # frozen_string_literal: true
 
 class PilotsController < ApplicationController
+  def create
+    ActiveRecord::Base.transaction do
+      pilot = Pilot.find_or_create_by(
+        pilot_certification: pilots_params[:pilot_certification],
+        name: pilots_params[:name],
+        age: pilots_params[:age],
+        credits: pilots_params[:credits],
+        location_planet: pilots_params[:location_planet]
+      )
+
+      ship = pilot.create_ship(
+        fuel_capacity: ship_params[:fuel_capacity],
+        fuel_level: ship_params[:fuel_level],
+        weight_capacity: ship_params[:weight_capacity]
+      )
+
+      if ship.persisted? && pilot.update(ship_id: ship.id)
+        render json: pilot, status: :created
+      else
+        render json: { errors: pilot.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  end
+
+  private
+
+  def pilots_params
+    params.require(:pilot).permit(:pilot_certification, :name, :age, :credits, :location_planet)
+  end
+
+  def ship_params
+    params.require(:ship).permit(:fuel_capacity, :fuel_level, :weight_capacity)
+  end
 end
