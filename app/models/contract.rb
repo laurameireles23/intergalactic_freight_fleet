dbff39a4a30fa344b8ade46266e7c143a50b97cc
Laura Meireles <laura.meireles1@gmail.com>
2024-09-04 14:48:06 -0300

    Feat: Create a enum, validates and adding belongs to from contract

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class Contract < ApplicationRecord
+  enum status: { pending: 0, in_progress: 1, completed: 2, canceled: 3 }
+
+  validates :status, inclusion: { in: statuses.keys }
+  validates :description, :origin_planet, :destination_planet, :value, :status, presence: true
+
+  belongs_to :resource
+  belongs_to :pilot
 end
