e0c4a7ccc5ce0a56fcdc4804d4b263a051189de3
Laura Meireles <laura.meireles1@gmail.com>
2024-09-05 23:20:22 -0300

    Feat: Creating new methods and tests from ship model

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -4,4 +4,16 @@ class Ship < ApplicationRecord
   belongs_to :pilot
 
   validates :fuel_capacity, :fuel_level, :weight_capacity, presence: true
+
+  def can_load?(load_weight)
+    weight_capacity >= load_weight
+  end
+
+  def enough_fuel?(fuel_needed)
+    fuel_level >= fuel_needed
+  end
+
+  def consume_fuel(fuel_used)
+    update!(fuel_level: fuel_level - fuel_used)
+  end
 end
--- spec/models/ship_spec.rb
+++ spec/models/ship_spec.rb
@@ -38,4 +38,58 @@ RSpec.describe Ship, type: :model do
       expect(ship).to_not be_valid
     end
   end
+
+  describe "with a created ship" do
+    let(:pilot) do
+      Pilot.create(
+        pilot_certification: '123456-x',
+        name: 'John Doe',
+        age: 35,
+        credits: 100,
+        location_planet: 'Andvari'
+      )
+    end
+
+    let!(:ship) do
+      Ship.create(
+        fuel_capacity: 100,
+        fuel_level: 50,
+        weight_capacity: 100,
+        pilot: pilot
+      )
+    end
+
+    before do
+      pilot.update!(ship: ship)
+    end
+
+    describe '#can_load?' do
+      it 'returns true if the ship can load the weight' do
+        expect(ship.can_load?(20)).to be true
+      end
+
+      it 'returns false if the ship cannot load the weight' do
+        expect(ship.can_load?(200)).to be false
+      end
+    end
+
+    describe '#enough_fuel?' do
+      it 'returns true if the ship has enough fuel' do
+        expect(ship.enough_fuel?(30)).to be true
+      end
+
+      it 'returns false if the ship does not have enough fuel' do
+        expect(ship.enough_fuel?(60)).to be false
+      end
+    end
+
+    describe '#consume_fuel' do
+      it 'reduces the fuel level by the specified amount' do
+        ship.consume_fuel(20)
+        ship.reload
+
+        expect(ship.fuel_level).to eq(30)
+      end
+    end
+  end
 end
