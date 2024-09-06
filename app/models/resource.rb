251f560c75e0dce2e2914f11adbd55ad25cba611
Laura Meireles <laura.meireles1@gmail.com>
2024-09-06 10:42:17 -0300

    Fix: Changing permitted values to resource name

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class Resource < ApplicationRecord
+  VALID_RESOURCE_NAME = ['minerals', 'water', 'food']
+
   has_many :contract
 
   validates :name, :weight, presence: true
+  validates :name, inclusion: { in: VALID_RESOURCE_NAME }
 end
