4f5f7447fc6c70afc4e9251043deff811249f2eb
Laura Meireles <laura.meireles1@gmail.com>
2024-09-06 10:42:46 -0300

    Feat: Creating new tests from new validation

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -4,19 +4,8 @@ require 'rails_helper'
 
 RSpec.describe Resource, type: :model do
   describe 'validations' do
-    let(:valid_resource) do
-      Resource.new(
-        name: 'Resource Name',
-        weight: 500
-      )
-    end
-
-    let(:invalid_resource) do
-      Resource.new(
-        name: '',
-        weight: nil
-      )
-    end
+    let(:valid_resource) { Resource.new(name: 'water', weight: 500) }
+    let(:invalid_resource) { Resource.new(name: 'invalid', weight: 12) }
 
     it 'is valid with valid attributes' do
       expect(valid_resource).to be_valid
@@ -30,6 +19,34 @@ RSpec.describe Resource, type: :model do
     it 'is not valid without a weight' do
       invalid_resource.weight = nil
       expect(invalid_resource).to_not be_valid
+    end
+
+    context 'when the resource name is valid' do
+      it 'is valid with a valid name' do
+        valid_resource.name = 'water'
+        expect(valid_resource).to be_valid
+      end
+
+      it 'is valid with another valid name' do
+        valid_resource.name = 'minerals'
+        expect(valid_resource).to be_valid
+      end
+
+      it 'is valid with a third valid name' do
+        valid_resource.name = 'food'
+        expect(valid_resource).to be_valid
+      end
+    end
+
+    context 'when the resource name is invalid' do
+      it 'is not valid with an invalid name' do
+        expect(invalid_resource).to_not be_valid
+      end
+
+      it 'adds an error for invalid name' do
+        invalid_resource.valid?
+        expect(invalid_resource.errors[:name]).to include('is not included in the list')
+      end
     end
   end
 end
